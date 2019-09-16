bd5b3023478294d9a8181bf9586e130da6b74df0
Greyflanel <richardlovelace811@gmail.com>
2019-09-15 20:11:35 -0500

    Added an iframe tag in the VideoDetail component, which takes in a variable at the end of a url. This variable is the embed code for each video. This makes it possible to play the selected video immediately after the video is selected.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import youtube from '../api/youtube';
 import React from 'react';
 import SearchBar from './SearchBar';
-import VideoList from './VideoList'
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
 
-    state = { videos: [] }
+    state = { videos: [],
+    selectedVideo: null }
 
     onTermSubmit = async term => {
 
@@ -14,12 +16,18 @@ class App extends React.Component {
                 q: term
             }
         });
+
        this.setState({ videos: response.data.items });
     };
 
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video });
+    }
+
 render() {
     return <div className="ui container"><SearchBar onFormSubmit={this.onTermSubmit}/>
-    <VideoList videos={this.state.videos}/>
+    <VideoDetail video={this.state.selectedVideo} />
+    <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
     </div>
 }
 };
