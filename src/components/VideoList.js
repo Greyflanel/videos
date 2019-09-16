b050fc71b9d07c38c6867a0eb3a485663afb27f5
Greyflanel <richardlovelace811@gmail.com>
2019-09-15 22:45:19 -0500

    Added componentDidMount to make a default search term in order to avoid blank space. Also, set seleted video on the first video in the array of videos, again, in order to avoid blank space. The app is complete.

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,9 +3,8 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => {
    const renderedList = videos.map( video  => {
-        return <VideoItem onVideoSelect={onVideoSelect} video={video} key={video.snippet.title} />
+        return <VideoItem onVideoSelect={onVideoSelect} video={video} key={video.id.videoId} />
     });
-   console.log({renderedList});
    
     return <div className="ui relaxed divided list">{renderedList}</div>
 };
